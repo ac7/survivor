ef361132ba7a3d3d9173ce3184fbb4635ef4e5b1
ac7 <ac7@users.noreply.github.com>
2014-07-20 17:52:25 -0500

    Update configuration file

--- conf.lua
+++ conf.lua
@@ -1,6 +1,13 @@
 
 function love.conf(t)
+	t.version = "0.9.1"
+
+	t.window.title = "survivor"
 	t.window.width = 960
 	t.window.height = 540
+	t.window.fullscreentype = "desktop"
+
+	t.modules.physics = false
+	t.modules.joystick = false
 end
 
