a7d767b106e9bfc485c9baca31781f0c98c5ca09
ac7 <ac7@users.noreply.github.com>
2014-07-20 18:01:45 -0500

    Write Entity constructor and update methods

--- entity.lua
+++ entity.lua
@@ -2,16 +2,39 @@
 Entity = class{
 	x = 0,
 	y = 0,
+	angle = 0,
+	speed = 1,
 }
 
+function Entity:__init(x, y)
+	assert(type(x) == "number")
+	assert(type(y) == "number")
+	self.x = x
+	self.y = y
+end
+
 function Entity:update(dt)
+	self.x = self.x + math.sin(self.angle) * self.speed * dt
+	self.y = self.y + math.cos(self.angle) * self.speed * dt
 end
 
 function Entity:draw()
 end
 
-function testEntity()
-	local e = Entity()
+function testEntityMovement()
+	local e = Entity(1, 0)
 	assert(type(e.update) == "function", "Entity doesn't have an update method")
+
+	e.angle = 0
+	e.speed = 4
+
+	e:update(0.5)
+	assert(e.y == 2, "Entity didn't move as expected.")
+	e.angle = math.pi / 2
+	e:update(0.5)
+	assert(e.x == 3, "Entity didn't move as expected.")
+	e.angle = -math.pi / 2
+	e:update(0.5)
+	assert(e.x == 1, "Entity didn't move as expected.")
 end
 
