38432d2b00003c57102c081f09f79a83bc346bbd
ac7 <ac7@users.noreply.github.com>
2014-07-20 20:08:37 -0500

    Update formatting in test framework

--- test.lua
+++ test.lua
@@ -3,6 +3,7 @@ enableTesting = true
 
 function runTests()
 	local count = 0
+	print("Testing: Running test cases")
 	for k, v in pairs(_G) do
 		local start, _ = string.find(k, "test")
 		if start == 1 then
@@ -10,12 +11,12 @@ function runTests()
 			assert(type(v) == "function", "Global variable prefixed with test is not a function.")
 			local status, msg = pcall(v)
 			if not status then
-				error(("\n%s FAIL: %s"):format(k, msg))
+				error(("\n  %s FAIL: %s"):format(k, msg))
 			else
-				print(("%-30s OK"):format(k))
+				print(("  %-50s OK"):format(k))
 			end
 		end
 	end
-	print("Testing: All " .. count .. " tests passed.")
+	print("Testing: All " .. count .. " tests passed")
 end
 
