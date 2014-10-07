24d78e2abd4dd53d7ecc15e568329f11a87442c1
ac7 <ac7@users.noreply.github.com>
2014-10-06 22:51:02 -0500

    Comment on runTests()
    Also print error messages without panicing. This way you can see all the tests
    that have passed and failed instead of halting after the first failure.

--- test.lua
+++ test.lua
@@ -1,6 +1,11 @@
 
+-- runTests goes through every function in the global namespace and runs the
+-- functions that begin with `test`. The general idea was adopted from the test
+-- framework in Golang (golang.org). These functions can use `assert` because any
+-- thrown errors will be caught and printed.
 function runTests()
 	local count = 0
+	local passed = 0
 	print("Testing: Running test cases")
 	for k, v in pairs(_G) do
 		local start, _ = string.find(k, "test")
@@ -11,12 +16,13 @@ function runTests()
 
 			local status, msg = pcall(v)
 			if not status then
-				error(("\n  %s FAIL: %s"):format(k, msg))
+				print(("  %-50s FAIL\n\t%s"):format(k, msg))
 			else
-				print(("  %-50s OK"):format(k))
+				print(("  %-50s ok"):format(k))
+				passed = passed + 1
 			end
 		end
 	end
-	print("Testing: All " .. count .. " tests passed")
+	print("Testing: " .. passed .. "/" .. count .. " passed")
 end
 
