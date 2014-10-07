0b9033af438f050e516d198b3cdd0914b208c59f
ac7 <ac7@users.noreply.github.com>
2014-10-06 20:59:37 -0500

    Build working keyboard movement

--- desmond.lua
+++ desmond.lua
@@ -7,9 +7,23 @@ function Desmond:draw()
 	love.graphics.rectangle("fill", drawX, drawY, 50, 50)
 end
 
-function Desmond:keypressed(key, unicode)
+function Desmond:update(dt)
+	local movX, movY = 0, 0
+
+	if love.keyboard.isDown("down") then movY = 1; end
+	if love.keyboard.isDown("right") then movX = 1; end
+	if love.keyboard.isDown("left") then movX = -1; end
+	if love.keyboard.isDown("up") then movY = -1; end
+
+	if movX ~= 0 or movY ~= 0 then
+		self.speed = 200
+	else self.speed = 0; end
+
+	self.angle = math.atan2(movX, movY)
+
+	self.super.update(self, dt)
 end
 
-function Desmond:keyreleased(key, unicode)
-end
+function Desmond:keypressed(key, unicode) end
+function Desmond:keyreleased(key, unicode) end
 
