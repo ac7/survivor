34ff732432068a984b05442d6dee65c3ac0f8dcf
ac7 <ac7@users.noreply.github.com>
2014-10-14 14:38:50 -0500

    Introduce `State` class
    This is the first step towards making a more object-oriented state system.

--- state.lua
+++ state.lua
@@ -1,4 +1,19 @@
 
+State = class{
+	-- `entities` cannot be initialized to a table because otherwise the
+	-- same table would be shared by all descendents of `State`. Thanks Lua.
+	entities = nil,
+}
+
+function State:setup() end
+
+function State:update(dt)
+	assertIs("table", self.entities)
+	for _, v in pairs(self.entities) do
+		v:update(dt)
+	end
+end
+
 function setState(newState)
 	-- don't set state to newState until `setup()` has been called in case
 	-- newState needs to reference the current state for some reason
