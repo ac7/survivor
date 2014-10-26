d2502e4b0391eca620141deeb0276de9bfe0ef90
ac7 <ac7@users.noreply.github.com>
2014-10-25 23:03:56 -0500

    Improve Comrade unit test

--- comrade_test.lua
+++ comrade_test.lua
@@ -7,11 +7,14 @@ function testComradeUpdate()
 	assertEq(-9, c.targetY)
 	assert(c.moving == true)
 
-	for i=0,100 do
-		c:update(0.0005)
+	for i=0,5 do
+		c:update(1)
 	end
 
 	assertEq(-14, math.floor(c.x+0.5))
 	assertEq(-9, math.floor(c.y+0.5))
+
+	assert(c.moving == false,
+		"Comrade should have stopped moving after reaching destination")
 end
 
