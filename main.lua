378f218467fb564fae163835ffe47c37043e27ee
ac7 <ac7@users.noreply.github.com>
2014-10-26 10:09:33 -0500

    Reformat some lines in main.lua

--- main.lua
+++ main.lua
@@ -3,6 +3,7 @@ function love.load()
 	require "imports"
 
 	local play = Play:new()
+
 	desmond = Desmond:new()
 	table.insert(play.entities, desmond)
 	table.insert(play.entities, desmond.comrade)
@@ -10,17 +11,13 @@ function love.load()
 	table.insert(play.entities, Square(-256, 0, {255, 0, 0}))
 	table.insert(play.entities, Square(-96, 128, {255, 0, 0}))
 	table.insert(play.entities, Square(96, 128, {255, 0, 0}))
+
 	play:track(desmond)
 	setState(play)
 end
 
-function love.update(dt)
-	state:update(dt)
-end
-
-function love.draw()
-	state:draw(dt)
-end
+function love.update(dt) state:update(dt); end
+function love.draw()     state:draw(dt); end
 
 function love.keypressed(key, unicode)
 	desmond:keypressed(key, unicode)
